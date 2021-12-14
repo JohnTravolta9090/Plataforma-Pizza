855a157c9e38d028dc686f962fe485d41dcd13d6
JohnTravolta909 <heitormicheli7@gmail.com>
2021-12-14 20:28:56 -0300

    Limpar firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,12 @@
 //Configuração do firebase - Direcionar para o projeto certo
 const firebaseConfig = {
-    apiKey: "AIzaSyCtjoqZhejTcWmiuYs4ROSJSsTy-wqLvYY",
-    authDomain: "plataformapizza.firebaseapp.com",
-    projectId: "plataformapizza",
-    storageBucket: "plataformapizza.appspot.com",
-    messagingSenderId: "363663914755",
-    appId: "1:363663914755:web:150c86aa0c762f1c6e5b36",
-    measurementId: "G-1NC277TS3F"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 //Inicializar o firebase
